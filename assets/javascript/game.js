fc7520e2921adb05e2308d30c420a60f8dd22803
Jesse Thomas <jayayetea@gmail.com>
2017-02-21 21:04:14 -0600

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,98 +4,60 @@ window.onload = function () {
 	var wins = 0;
 	var losses = 0;
 	var crystals = [0,0,0,0];
-		
-	function randomNumbers(){
-		randNum = Math.floor(Math.random()*(120-19)+19)
-		$("#crystalNumber").text(randNum);
 
-		for (var i=0;i<4;i++){
-			randCrystal = Math.floor(Math.random()*(12-1)+1);
-			console.log(randCrystal);
-			crystals[i] = randCrystal;
-			console.log(crystals[i]);
+	function checkScore(){
+		if (score == randNum){
+			alert("Yay! You won! Let's play again!");
+			wins += 1;
+			score = 0;
+			randomNumbers();
+			displayScore();
+		}
+		if (score > randNum){
+			alert("Sorry! You lose. Let's play again!");
+			losses += 1;
+			score = 0;
+			randomNumbers();
+			displayScore();
 		}
 	};
 
 	function displayScore(){
 		$("#scoreKeeper").html("Wins: "+wins+"<br>Losses: "+losses);
-		$("#totalScore").html("Your total score is: "+score);
+		$("#totalScore").html(score);
+	};
+
+	function randomNumbers(){
+		randNum = Math.floor(Math.random()*(120-19)+19)
+		$("#crystalNumber").text(randNum);
+
+		for (var i=0;i<crystals.length;i++){
+			randCrystal = Math.floor(Math.random()*(12-1)+1);
+			crystals[i] = randCrystal;
+		}
 	};
 
 	randomNumbers();
 	displayScore();
 
 	$("#crystalOne").on("click", function(event){
-		console.log(crystals[0])
 		score += crystals[0];
 		displayScore();
-		if (score == randNum){
-			alert("Yay! You won! Let's play again!");
-			wins += 1;
-			score = 0;
-			randomNumbers();
-			displayScore();
-		}
-		if (score > randNum){
-			alert("Sorry! You lose. Let's play again!");
-			losses += 1;
-			score = 0;
-			randomNumbers();
-			displayScore();
-		}
+		checkScore();
 	});
 	$("#crystalTwo").on("click", function(event){
 		score += crystals[1];
 		displayScore();
-		if (score == randNum){
-			alert("Yay! You won! Let's play again!");
-			wins += 1;
-			score = 0;
-			randomNumbers();
-			displayScore();
-		}
-		if (score > randNum){
-			alert("Sorry! You lose. Let's play again!");
-			losses += 1;
-			score = 0;
-			randomNumbers();
-			displayScore();
-		}
+		checkScore();
 	});
 	$("#crystalThree").on("click", function(event){
 		score += crystals[2];
 		displayScore();
-		if (score == randNum){
-			alert("Yay! You won! Let's play again!");
-			wins += 1;
-			score = 0;
-			randomNumbers();
-			displayScore();
-		}
-		if (score > randNum){
-			alert("Sorry! You lose. Let's play again!");
-			losses += 1;
-			score = 0;
-			randomNumbers();
-			displayScore();
-		}	
+		checkScore();
 	});
 	$("#crystalFour").on("click", function(event){
 		score += crystals[3];
 		displayScore();
-		if (score == randNum){
-			alert("Yay! You won! Let's play again!");
-			wins += 1;
-			score = 0;
-			randomNumbers();
-			displayScore();
-		}
-		if (score > randNum){
-			alert("Sorry! You lose. Let's play again!");
-			losses += 1;
-			score = 0;
-			randomNumbers();
-			displayScore();
-		}	
+		checkScore();
 	});
 };
